c2e78154c037d14ef7ab58b8a2ab2874b1aea902
Zin Nwe Oo <zinnaw413@gmail.com>
2021-09-02 16:57:45 -0700

    my proj

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -7,10 +7,10 @@ class Applicant < ApplicationRecord
     # validates :phone_no2, :numericality => true, length: {maximum: 12}
     validates :email, presence:true, uniqueness:true, length:{ in:10..100 } ,format: { with: /.*@.*/}
     validates :currnet_address, presence:true, length: {maximum: 250 }
-    validates :hometown_address, presence:true, length: {maximum: 250}
-    validates :bachelor_university, length: {maximum: 100}
-    validates :bachelor_year,length: {maximum: 20}
-    validates :bachelor_degree,length: {maximum: 100}
+    validates :hometown_address, length: {maximum: 250}
+    validates :bachelor_university,  presence:true, length: {maximum: 100}
+    validates :bachelor_year, presence:true, length: {maximum: 20}
+    validates :bachelor_degree,  presence:true, length: {maximum: 100}
     validates :master_university,length: {maximum: 100}
     validates :master_graduate_year, length: {maximum: 100}
     validates :master_degree,length: {maximum: 100}
